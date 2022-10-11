4943f123855287b24c12dcdf24d85a0786577406
ivsonramos <ivson.ramos@gmail.com>
2022-10-10 22:04:07 -0300

    :construction:Adiciona componentes PrimeNG

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import {ButtonModule} from 'primeng/button';
+import {InputTextModule} from 'primeng/inputtext';
 
 @NgModule({
   declarations: [
@@ -21,7 +22,8 @@ import {ButtonModule} from 'primeng/button';
     HttpClientModule,
     BrowserAnimationsModule,
     FormsModule,
-    ButtonModule
+    ButtonModule,
+    InputTextModule
   ],
   providers: [],
   bootstrap: [HomeComponent]
